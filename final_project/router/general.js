fefae1ec909b6cc68d0154c650a82e21ea1738b5
JAvino22 <jenb52@gmail.com>
2024-09-16 18:29:58 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,7 +9,7 @@ public_users.post("/register", (req,res) => {
     const password = req.body.password;
     if (username && password) {
         if (!isValid(username)) {
-            users.push({"username":username,"password":password});
+            users.push({"username":user2,"pass2":password});
             return res.status(200).json({message:`User ${username} registered`});
         }
         else {
@@ -53,30 +53,37 @@ public_users.get('/isbn/:isbn', function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    getBooks()
-    .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.author === author))
-    .then((filteredBooks) => res.send(filteredBooks));
-});
+    let booksbyauthor = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["author"] === req.params.author) {
+        booksbyauthor.push({"isbn":isbn,
+                            "title":books[isbn]["title"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbyauthor}, null, 4));
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    getBooks()
-    .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.title === title))
-    .then((filteredBooks) => res.send(filteredBooks));
-});
+    let booksbytitle = [];
+    let isbns = Object.keys(books);
+    isbns.forEach((isbn) => {
+      if(books[isbn]["title"] === req.params.title) {
+        booksbytitle.push({"isbn":isbn,
+                            "author":books[isbn]["author"],
+                            "reviews":books[isbn]["reviews"]});
+      }
+    });
+    res.send(JSON.stringify({booksbytitle}, null, 4));
+  });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  getByISBN(req.params.isbn)
-  .then(
-      result => res.send(result.reviews),
-      error => res.status(error.status).json({message: error.message})
-  );
-});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]["reviews"])
+  });
 
-module.exports.general = public_users;lic_users;
+module.exports.general = public_users;
